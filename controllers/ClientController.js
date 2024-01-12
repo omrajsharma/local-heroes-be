0eb30d0faad7336a0b1fe505d697edf29fec3d9e
omrajsharma <omraj7413@gmail.com>
2024-01-12 08:30:11 +0530

    feat: client booking

--- controllers/ClientController.js
+++ controllers/ClientController.js
@@ -1,4 +1,6 @@
 const UserModel = require('../models/UserModel')
+const BookingModel = require('../models/BookingModel')
+const jwt = require('jsonwebtoken');
 
 const getProvidersByCategory = async (req, res) => {
     const category = req.query.category;
@@ -21,4 +23,37 @@ const getProvidersByCategory = async (req, res) => {
     }
 }
 
-module.exports = {getProvidersByCategory}
+const createBooking = async (req, res) => {
+    const token = req.cookies.token;
+    console.log(token);
+    const {providerId, providerServiceId, date, startTime, endTime, address, paymentMode} = req.body;
+    console.log(req.body);
+
+    // validation to be added
+
+    try {
+        const tokenInfo = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const clientUserDoc = await UserModel.findById(tokenInfo.id)
+        const providerUserDoc = await UserModel.findById(providerId)
+        const service = providerUserDoc.services.find(service => service._id.toString() === providerServiceId);
+
+        const bookingDoc = await BookingModel.create({
+            providerId: providerUserDoc._id,
+            providerServiceId: service._id,
+            clientId: clientUserDoc._id,
+            status: "IN_PROGRESS",
+            clientAddress: address,
+            date: date,
+            startTime: startTime,
+            endTime: endTime,
+            paymentMode: paymentMode
+        })
+
+        res.status(201).json(bookingDoc)
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ error: "Something went wrong" })
+    }
+}
+
+module.exports = {getProvidersByCategory, createBooking}
---
+++ models/BookingModel.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+const BookingSchema = new mongoose.Schema({
+    providerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "UserModel",
+        required: true
+    },
+    providerServiceId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "UserModel",
+        required: true
+    },
+    clientId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "UserModel",
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["IN_PROGRESS", "COMPLETED", "CANCELLED"],
+        required: [true, "Empty UserType"]
+    },
+    clientAddress: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: false
+    },
+    startTime: {
+        type: String,
+        required: false
+    },
+    endTime: {
+        type: String,
+        required: false
+    },
+    paymentMode: {
+        type: String,
+        enum: ["UPI", "COD"],
+        required: [true, "Empty payment mode"]
+    }
+},
+{
+    timestamps: true
+})
+
+module.exports = mongoose.model('bookings', BookingSchema);
